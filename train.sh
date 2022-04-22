cd13934e0d1f1504a646ac846669ec4d7e7a3d07
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2022-04-22 17:40:03 +0100

    Test instances increated to 10k

--- train.sh
+++ train.sh
@@ -3,9 +3,9 @@
 for dataset_size in 10 20 30 40 50 60 70 80 90 100 250 500 750 1000; do
     for k in 2 3 4 5 6 7 8 9; do
         echo "Training the model..."
-        python3 train.py --exp_name "exp_01" --num-training-examples ${dataset_size} --num-test-examples 1000 --k ${k}
+        python3 train.py --exp_name "exp_01" --num-training-examples ${dataset_size} --num-test-examples 10000 --k ${k}
 
         echo "Evaluating the model..."
-        python3 train.py --exp_name "exp_01" --num-training-examples ${dataset_size} --num-test-examples 1000 --k ${k} --eval True
+        python3 train.py --exp_name "exp_01" --num-training-examples ${dataset_size} --num-test-examples 10000 --k ${k} --eval True
     done
 done
