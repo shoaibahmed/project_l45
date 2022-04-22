423cad9428623b84eb6746641ff56c5795e89107
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2022-04-22 18:55:09 +0100

    Plotting utils updated

--- plot_results.py
+++ plot_results.py
@@ -1,16 +1,43 @@
 import os
 import glob
 
+import json
 import numpy as np
 import pandas as pd
 
-# relevant_files = "./outputs/exp_01_train_ex_[0-9]+_k_[0-9]+_pos/models/training_dynamics.csv"
 relevant_files = "./*/*/*/training_dynamics.csv"
 files = glob.glob(relevant_files)
 print(len(files), files[:3])
 
+output_dict = {}
 for f in files:
     print("Reading file:", f)
     df = pd.read_csv(f)
-    print(df.tail(1))
-    exit()
+    final_results = df.tail(1)
+    train_mse, test_mse = float(final_results["train_loss"]), float(final_results["test_loss"])
+    print(final_results, train_mse, test_mse)
+    
+    # Get config from the model file
+    file_parts = f.split(os.sep)
+    config = file_parts[-3].split("_")  # file, model, config, outputs
+    print(config)
+    
+    assert config[-1] == "pos"
+    assert config[-3] == "k"
+    k = int(config[-2])
+    
+    assert config[-6] == "train"
+    assert config[-5] == "ex"
+    num_examples = int(config[-4])
+    print(f"k: {k} / num examples: {num_examples}")
+    
+    if num_examples not in output_dict:
+        output_dict[num_examples] = {}
+    assert k not in output_dict[num_examples]
+    output_dict[num_examples][k] = (train_mse, test_mse)
+
+print(json.dumps(output_dict, indent=4, sort_keys=True))
+
+# TODO: Plot the dictionary here
+
+# TODO: Plot the intervention results here
