415fa40c37a8439a0800e6c81c7eb3b3be9b1cef
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2022-04-23 19:03:24 +0100

    Plotting scheme updated

--- plot_results.py
+++ plot_results.py
@@ -32,63 +32,63 @@ relevant_files = "./*/*/*/training_dynamics.csv"
 files = glob.glob(relevant_files)
 print(len(files), files[:3])
 
-output_dict = {}
-for f in files:
-    print("Reading file:", f)
-    df = pd.read_csv(f)
-    final_results = df.tail(1)
-    train_mse, test_mse = float(final_results["train_loss"]), float(final_results["test_loss"])
-    print(final_results, train_mse, test_mse)
-    
-    k, num_examples = get_k_train_ex_from_file(f)
-    print(f"k: {k} / num examples: {num_examples}")
-    
-    if num_examples not in output_dict:
-        output_dict[num_examples] = {}
-    assert k not in output_dict[num_examples]
-    output_dict[num_examples][k] = {"train": train_mse, "test": test_mse}
-
-# print(json.dumps(output_dict, indent=4, sort_keys=True))
-
-# Plot the dictionary here
-num_training_examples = natsort.natsorted(list(output_dict.keys()))
-print("Number of training examples:", num_training_examples)
-
-k_settings = natsort.natsorted(list(output_dict[num_training_examples[0]].keys()))
-print("k-NN configurations:", k_settings)
-
-# Validate that the k settings are same for every run
-assert all([all([natsort.natsorted(list(output_dict[num_training_examples[i]].keys())) == k_settings]) for i in range(len(num_training_examples))])
-
-# Compute the same limits
-max_val = None
-for training_examples in num_training_examples:
-    k_nn_mse = [output_dict[training_examples][k]["test"] for k in k_settings]
-    if max_val is None:
-        max_val = max(k_nn_mse)
-    else:
-        max_val = max(max_val, max(k_nn_mse))
-
 output_dir = "plots/"
 if not os.path.exists(output_dir):
     print("Created output directory...")
     os.mkdir(output_dir)
 
-for training_examples in num_training_examples:
-    plt.figure(figsize=(6, 6))
-    
-    bar_width = 0.5
-    k_nn_mse = [output_dict[training_examples][k]["test"] for k in k_settings]
-    plt.bar(range(len(k_settings)), k_nn_mse, width=bar_width, color = 'orange', edgecolor = 'black')
-    
-    plt.xticks(np.arange(len(k_settings)), k_settings)
-    plt.ylabel('MSE', fontsize=14)
-    plt.xlabel('k for k-NN', fontsize=14)
-    plt.title(f"Impact of k with {training_examples} training examples", fontsize=14)
-    plt.ylim(0, max_val)
+plot_knn_results = False
+if plot_knn_results:
+    output_dict = {}
+    for f in files:
+        print("Reading file:", f)
+        df = pd.read_csv(f)
+        final_results = df.tail(1)
+        train_mse, test_mse = float(final_results["train_loss"]), float(final_results["test_loss"])
+        print(final_results, train_mse, test_mse)
+        
+        k, num_examples = get_k_train_ex_from_file(f)
+        print(f"k: {k} / num examples: {num_examples}")
+        
+        if num_examples not in output_dict:
+            output_dict[num_examples] = {}
+        assert k not in output_dict[num_examples]
+        output_dict[num_examples][k] = {"train": train_mse, "test": test_mse}
 
-    plt.tight_layout()
-    plt.savefig(os.path.join(output_dir, f"results_k_nn_{training_examples}_train_ex.png"), dpi=300)
+    # Plot the dictionary here
+    num_training_examples = natsort.natsorted(list(output_dict.keys()))
+    print("Number of training examples:", num_training_examples)
+
+    k_settings = natsort.natsorted(list(output_dict[num_training_examples[0]].keys()))
+    print("k-NN configurations:", k_settings)
+
+    # Validate that the k settings are same for every run
+    assert all([all([natsort.natsorted(list(output_dict[num_training_examples[i]].keys())) == k_settings]) for i in range(len(num_training_examples))])
+
+    # Compute the same limits
+    max_val = None
+    for training_examples in num_training_examples:
+        k_nn_mse = [output_dict[training_examples][k]["test"] for k in k_settings]
+        if max_val is None:
+            max_val = max(k_nn_mse)
+        else:
+            max_val = max(max_val, max(k_nn_mse))
+
+    for training_examples in num_training_examples:
+        plt.figure(figsize=(6, 6))
+        
+        bar_width = 0.5
+        k_nn_mse = [output_dict[training_examples][k]["test"] for k in k_settings]
+        plt.bar(range(len(k_settings)), k_nn_mse, width=bar_width, color = 'orange', edgecolor = 'black')
+        
+        plt.xticks(np.arange(len(k_settings)), k_settings)
+        plt.ylabel('MSE', fontsize=14)
+        plt.xlabel('k for k-NN', fontsize=14)
+        plt.title(f"Impact of k with {training_examples} training examples", fontsize=14)
+        plt.ylim(0, max_val)
+
+        plt.tight_layout()
+        plt.savefig(os.path.join(output_dir, f"results_k_nn_{training_examples}_train_ex.png"), dpi=300)
 
 # TODO: Plot the intervention results here
 relevant_files = "./*/*/*/intervention_test.csv"
@@ -100,46 +100,85 @@ if not os.path.exists(output_dir):
     print("Created output directory...")
     os.mkdir(output_dir)
 
-output_dict = {}
-for f in files:
-    print("Reading file:", f)
-    df = pd.read_csv(f)
-    print(df)
-    
-    k, num_examples = get_k_train_ex_from_file(f)
-    print(f"k: {k} / num examples: {num_examples}")
-    
-    if num_examples not in output_dict:
-        output_dict[num_examples] = {}
-    assert k not in output_dict[num_examples]
-    output_dict[num_examples][k] = df
-    
-    plt.figure(figsize=(10, 6))
-    
-    intervention_nodes = df["intervention_node"].to_numpy().tolist()
-    all_nodes = [col.replace("diff_", "") for col in df.columns if col != "intervention_node"]
-    del df["intervention_node"]
-    
-    print("All nodes:", all_nodes)
-    print("Intervention nodes:", intervention_nodes)
-    
-    intervention_results = df.to_numpy()
-    print(intervention_results)
-    
-    ax = sns.heatmap(intervention_results, annot=True, cmap='Blues', cbar=False)
+plot_intervention_results = True
+if plot_intervention_results:
+    for f in files:
+        print("Reading file:", f)
+        df = pd.read_csv(f)
+        print(df)
+        
+        k, num_examples = get_k_train_ex_from_file(f)
+        print(f"k: {k} / num examples: {num_examples}")
+        
+        plt.figure(figsize=(10, 6))
+        
+        intervention_nodes = df["intervention_node"].to_numpy().tolist()
+        all_nodes = [col.replace("diff_", "") for col in df.columns if col != "intervention_node"]
+        del df["intervention_node"]
+        
+        print("All nodes:", all_nodes)
+        print("Intervention nodes:", intervention_nodes)
+        
+        intervention_results = df.to_numpy()
+        print(intervention_results)
+        
+        ax = sns.heatmap(intervention_results, annot=True, cmap='Blues', cbar=False)
 
-    ax.set_title(f'Impact of intervention with model trained with k={k} and # training examples={num_examples}\n');
-    ax.set_xlabel('\n MSE between expected output and the model prediction')
-    ax.set_ylabel('Intervention on variable');
+        ax.set_title(f'Impact of intervention with model trained with k={k} and # training examples={num_examples}\n')
+        ax.set_xlabel('\n MSE between expected output and the model prediction')
+        ax.set_ylabel('Intervention on variable')
 
-    ## Ticket labels - List must be in alphabetical order
-    ax.xaxis.set_ticklabels(all_nodes)
-    ax.yaxis.set_ticklabels(intervention_nodes)
+        ax.xaxis.set_ticklabels(all_nodes)
+        ax.yaxis.set_ticklabels(intervention_nodes)
 
-    ## Display the visualization of the Confusion Matrix.
-    # plt.show()
-    plt.tight_layout()
-    plt.savefig(os.path.join(output_dir, f"results_k_{k}_{training_examples}_train_ex.png"), dpi=300, bbox_inches='tight', pad_inches=0.04)
+        plt.tight_layout()
+        plt.savefig(os.path.join(output_dir, f"results_k_{k}_{training_examples}_train_ex.png"), dpi=300, bbox_inches='tight', pad_inches=0.04)
 
 # TODO: Plot the attention results here
+relevant_files = "./*/*/*/attention_stats_layer_[0-4].csv"
+files = glob.glob(relevant_files)
+print(len(files), files[:3])
 
+output_dir = "plots/attention_plots/"
+if not os.path.exists(output_dir):
+    print("Created output directory...")
+    os.mkdir(output_dir)
+
+plot_attention_results = True
+if plot_attention_results:
+    for f in files:
+        print("Reading file:", f)
+        df = pd.read_csv(f)
+        print(df)
+        
+        k, num_examples = get_k_train_ex_from_file(f)
+        layer_idx = int(os.path.split(f)[1].replace(".csv", "").replace("attention_stats_layer_", ""))
+        print(f"k: {k} / num examples: {num_examples} / layer idx: {layer_idx}")
+        
+        plt.figure(figsize=(10, 6))
+        
+        main_node = df["node"].to_numpy().tolist()
+        all_nodes = [col.replace("connected_to_", "") for col in df.columns if col != "node"]
+        assert len(main_node) == len(all_nodes)
+        assert all([main_node[i] == all_nodes[i] for i in range(len(all_nodes))])
+        del df["node"]
+        print("All nodes:", all_nodes)
+        
+        connection_matrix = df.to_numpy().astype(np.int64)
+        print(connection_matrix)
+        num_elements = connection_matrix.diagonal()
+        assert all([num_elements[0] == num_elements[i] for i in range(len(num_elements))])
+        bs = num_elements[0]
+        connection_matrix_norm = connection_matrix.astype(np.float32) / bs
+        
+        ax = sns.heatmap(connection_matrix_norm, annot=True, cmap='Blues', cbar=False)
+
+        ax.set_title(f'Nearest neighbor matrix at layer {layer_idx+1} with model trained with k={k} and # training examples={num_examples}\n');
+        ax.set_xlabel('\n Nearest neighbor connection')
+        ax.set_ylabel('Node')
+
+        ax.xaxis.set_ticklabels(all_nodes)
+        ax.yaxis.set_ticklabels(all_nodes)
+
+        plt.tight_layout()
+        plt.savefig(os.path.join(output_dir, f"results_k_{k}_{num_examples}_train_ex_layer_{layer_idx}.png"), dpi=300, bbox_inches='tight', pad_inches=0.04)
