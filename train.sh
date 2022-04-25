9b97ca7e89defd54c73534c98f4fd674f0ed3036
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2022-04-25 19:37:41 +0100

    Bash script updated with new scm

--- train.sh
+++ train.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# scm=scm_easy
-scm=scm_difficult
+export CUDA_VISIBLE_DEVICES=0
+
+scm=scm_easy
 
 for dataset_size in 10 20 30 40 50 60 70 80 90 100 250 500 750 1000 2500 5000 7500 10000; do
     for k in 2 3 4 5 6 7 8 9 10; do
@@ -10,5 +11,17 @@ for dataset_size in 10 20 30 40 50 60 70 80 90 100 250 500 750 1000 2500 5000 75
 
         echo "Evaluating the model..."
         python3 train.py --exp_name "exp_01" --num-training-examples ${dataset_size} --num-test-examples 10000 --k ${k} --scm ${scm} --eval True
+    done
+done
+
+scm=scm_difficult
+
+for dataset_size in 10 20 30 40 50 60 70 80 90 100 250 500 750 1000 2500 5000 7500 10000; do
+    for k in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16; do
+        echo "Training the model..."
+        python3 train.py --exp_name "exp_02" --num-training-examples ${dataset_size} --num-test-examples 10000 --k ${k} --scm ${scm}
+
+        echo "Evaluating the model..."
+        python3 train.py --exp_name "exp_02" --num-training-examples ${dataset_size} --num-test-examples 10000 --k ${k} --scm ${scm} --eval True
     done
 done
