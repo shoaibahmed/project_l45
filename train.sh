4f1ba461f1e61a38b9ca8689525d5d1e8dc0dac2
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2022-04-25 02:12:33 +0100

    Updated training script

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for dataset_size in 10 20 30 40 50 60 70 80 90 100 250 500 750 1000; do
+for dataset_size in 10 20 30 40 50 60 70 80 90 100 250 500 750 1000 2500 5000 7500 10000; do
     for k in 2 3 4 5 6 7 8 9 10; do
         echo "Training the model..."
         python3 train.py --exp_name "exp_01" --num-training-examples ${dataset_size} --num-test-examples 10000 --k ${k}
